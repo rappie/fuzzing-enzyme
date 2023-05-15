437dfe3dd09af5b98f4daa2e7ae119ac1d270b20
Rappie <jasper@rappie.com>
2023-05-15 21:17:27 +0200

    testCreateList

--- src/echidna/EchidnaTest.sol
+++ src/echidna/EchidnaTest.sol
@@ -6,4 +6,27 @@ import "./EchidnaDebug.sol";
 import "./Debugger.sol";
 import "./Tools.sol";
 
-contract EchidnaTest is EchidnaSetup, EchidnaHelper, EchidnaDebug {}
+contract EchidnaTest is EchidnaSetup, EchidnaHelper, EchidnaDebug {
+    function testCreateList(
+        address owner,
+        uint8 _listType,
+        address[] memory _initialData,
+        bool startEmpty
+    ) public {
+        uint listId = registry.getListCount();
+
+        uint256 listCountBefore = registry.getListCount();
+        createList(owner, _listType, _initialData, startEmpty);
+        uint256 listCountAfter = registry.getListCount();
+
+        assert(listCountAfter == listCountBefore + 1);
+        assert(_getListType(_listType) == registry.getListUpdateType(listId));
+
+        if (!startEmpty) {
+            for (uint256 i = 0; i < _initialData.length; i++) {
+                assert(registry.isInList(listId, _initialData[i]));
+            }
+            assert(registry.areAllInList(listId, _initialData));
+        }
+    }
+}
