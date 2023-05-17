a47445f7f5d374be32732a00458478e3b35f72f1
Rappie <jasper@rappie.com>
2023-05-17 11:00:36 +0200

    testListCount

--- src/echidna/EchidnaDebug.sol
+++ src/echidna/EchidnaDebug.sol
@@ -7,10 +7,4 @@ contract EchidnaDebug is EchidnaHelper {
     // bool debug = true;
     bool debug = false;
 
-    function testListCount() public {
-        require(debug);
-        Debugger.log("list count", registry.getListCount());
-        assert(registry.getListCount() < 5);
-    }
-
 }
