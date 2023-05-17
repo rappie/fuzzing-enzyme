078f2f059795276df7e655131e0fd773d2628d81
Rappie <jasper@rappie.com>
2023-05-17 11:03:11 +0200

    internal

--- src/echidna/EchidnaSetup.sol
+++ src/echidna/EchidnaSetup.sol
@@ -8,6 +8,6 @@ import "../address-list-registry/AddressListRegistry.sol";
 contract EchidnaSetup is EchidnaConfig {
     IHevm hevm = IHevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
-    AddressListRegistry public registry =
+    AddressListRegistry internal registry =
         new AddressListRegistry(ADDRESS_DISPATCHER);
 }
